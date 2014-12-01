5b951083f97f1cc9b071273a935b6e9b4c1dc104
Miles <lynnprosper@gmail.com>
2014-12-02 00:11:52 +0800

    data03
    use "array" and append function

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
-highest_score = 0
+scores = []
 result_f = open("results.txt")
 for line in result_f:
-    (name,score) = line.split()
-    if float(score) > highest_score:
-        highest_score = float(score)
+    (name, score) = line.split()
+    scores.append(float(score))
 result_f.close()
-print("The highest score is:")
-print(highest_score)
-    
+print("The top scores were:")
+print(scores[0])
+print(scores[1])
+print(scores[2])
