a4873c2423269664e2bf24f0089443675f493451
Miles <lynnprosper@gmail.com>
2014-12-03 00:53:02 +0800

    data09
    fuction find

--- data.py
+++ data.py
@@ -1,4 +1,19 @@
-line = "101;Johnny 'wave-boy' Uones;usa;8.32;Fish;21"
-s = {}
-(s['id'],s['name'],s['country'],s['average'],s['board'],s['age']) = line.split(";")
-print('ID:  ' + s['id'])
+def find_details(id2find):
+    surfers_f = open("surfing_data.csv")
+    for each_line in surfers_f:
+        s = {}
+        (s['id'],s['name'],s['country'],s['average'],s['board'],s['age']) = each_line.split(";")
+        if id2find == int(s['id']):
+            surfers_f.close()
+            return(s)
+    surfers_f.close
+    return({})
+lookup_id = int(input("Enter the id of the surfer: "))
+surfer = find_details(lookup_id)
+if surfer:
+    print("ID:        " + surfer['id'])
+    print("Name:      " + surfer['name'])
+    print("Country:   " + surfer['country'])
+    print("Average:   " + surfer['average'])
+    print("Board:     " + surfer['board'])
+    print("Age:       " + surfer['age'])
