7bf27deb7cbbe75ef3ee330b76ea0e7f53712655
Miles <lynnprosper@gmail.com>
2014-12-06 00:45:22 +0800

    data10
    sql data

--- data.py
+++ data.py
@@ -1,16 +1,24 @@
+import sqlite3
 def find_details(id2find):
-    surfers_f = open("surfing_data.csv")
-    for each_line in surfers_f:
-        s = {}
-        (s['id'],s['name'],s['country'],s['average'],s['board'],s['age']) = each_line.split(";")
-        if id2find == int(s['id']):
-            surfers_f.close()
+    db = sqlite3.connect("surfersDB.sdb")
+    db.row_factory = sqlite3.Row
+    cursor = db.cursor()
+    cursor.execute("select * from surfers")
+    rows = cursor.fetchall()
+    for row in rows:
+        if row['id'] == id2find:
+            s = {}
+            s['id'] = str(row['id'])
+            s['name'] = row['name']
+            s['country'] = row['country']
+            s['average'] = str(row['average']
             return(s)
-    surfers_f.close
+    cursor.close()
     return({})
+            
 lookup_id = int(input("Enter the id of the surfer: "))
 surfer = find_details(lookup_id)
-if surfer:
+if len(surfer) > 0:
     print("ID:        " + surfer['id'])
     print("Name:      " + surfer['name'])
     print("Country:   " + surfer['country'])
