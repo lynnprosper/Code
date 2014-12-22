9afe30ff0dc34da27be3bde87ca45d54341da317
Miles <lynnprosper@gmail.com>
2014-12-23 00:49:45 +0800

    GUI02

--- GUI.py
+++ GUI.py
@@ -6,11 +6,26 @@ def wait_finish(channel):
     while channel.get_busy():
         pass
 
-s = sounds.Sound("heartbeat.wav")
-wait_finish(s.play())
-s2 = sounds.Sound("buzz.wav")
-wait_finish(s2.play())
-s3 = sounds.Sound("ohno.wav")
-wait_finish(s3.play())
-s4 = sounds.Sound("carhorn.wav")
-wait_finish(s4.play())
+correct_s = sounds.Sound("correct.wav")
+wrong_s= sounds.Sound("wrong.wav")
+prompt = "Press 1 for Correct, 2 for Wrong, or 0 to Quit:"
+number_asked = 0
+number_correct = 0
+number_wrong = 0
+
+choice = input(prompt)
+while choice != "0":
+    if choice == "1":
+        number_asked = number_asked + 1
+        number_correct = number_correct + 1
+        wait_finish(correct_s.play())
+    if choice == "2":
+        number_asked = number_asked + 1
+        number_wrong = number_wrong + 1
+        wait_finish(wrong_s.play())
+    choice = input(prompt)
+
+print("You asked " + str(number_asked) + " questions.")
+print(str(number_correct) + " were correctly answered.")
+print(str(number_wrong) + " were answered incorrectly.")
+    
