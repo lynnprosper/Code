71213955acf08489c772d648b8216bc8a90df014
Miles <lynnprosper@gmail.com>
2015-01-04 00:45:46 +0800

    GIE06

--- hfmix.pyw
+++ hfmix.pyw
@@ -8,10 +8,13 @@ app.title("Head First Mix")
 mixer = pygame.mixer
 mixer.init()
 
-create_gui(app, mixer, "50459_M_RED_Nephlimizer.wav")
-create_gui(app, mixer, "49119_M_RED_HardBouncer.wav")
+panel = SoundPanel(app, mixer, "50459_M_RED_Nephlimizer.wav")
+panel.pack()
+panel = SoundPanel(app, mixer, "49119_M_RED_HardBouncer.wav")
+panel.pack()
 
 def shutdown():
+    
     track.stop()
     app.destroy()
 
--- sound_panel.py
+++ sound_panel.py
@@ -1,27 +1,26 @@
 from tkinter import *
-import pygame
+import pygame.mixer
 
-def create_gui(app, mixer, sound_file):
-    def track_toggle():
-        if track_playing.get() == 1:
-            track.play(loops = -1)
-        else:
-            track.stop()
-
-    def change_volume(v):
-        track.set_volume(volume.get())
-
+class SoundPanel(Frame):
+    def _init_(self, app, mixer, sound_file):
+        Frame._init_(self, app)
+        self.track = mixer.Sound(sound_file)
+        self.track_playing = IntVar()
+        track_button = Checkbutton(self, variable = self.track_playing,
+                                   command = self.track_toggle, text = sound_file)
+        track_button.pack(side = LEFT)
+        self.volume = DoubleVar()
+        self.volume.set(self.track.get_volume())
+        volume_scale = Scale(self, variable = self.volume, from_ = 0.0, to = 1.0,
+                             resolution = 0.1, command = self.change_volume,
+                             label = "Volume", orient = HORIZONTAL)
+        volume_scale.pack(side = RIGHT)
     
+    def track_toggle(self):
+        if self.track_playing.get() == 1:
+            self.track.play(loops = -1)
+        else:
+            self.track.stop()
 
-    track = mixer.Sound(sound_file)
-    track_playing = IntVar()
-    track_button = Checkbutton(app, variable = track_playing,
-                               command = track_toggle, text = sound_file)
-    track_button.pack(side = LEFT)
-
-    volume = DoubleVar()
-    volume.set(track.get_volume())
-    volume_scale = Scale(variable = volume, from_ = 0.0, to = 1.0,
-                         resolution = 0.1, command = change_volume,
-                         label = "Volume", orient = HORIZONTAL)
-    volume_scale.pack(side = RIGHT)
+    def change_volume(self, v):
+        self.track.set_volume(self.volume.get())
